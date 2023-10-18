8e02dab66e745b0eb2e7d9f29ad5430f96419346
Андрей <vsscomp@mailru>
2023-10-18 22:44:12 +0300

    Change file README

--- src/main/java/consoleCalculator/StartClass.java
+++ src/main/java/consoleCalculator/StartClass.java
@@ -4,8 +4,13 @@ public class StartClass {
 
     public static void start() {
         System.out.println(
-                "The program is a console calculator." +
-                "\nOperations are selected by entering the values 1,2,3,4 from the keyboard" +
-                "\n1. Adding " + "\n2. Subtraction ");
+                """
+                        The program is a console calculator.
+                        Operations are selected by entering the values 1,2,3,4 from the keyboard
+                        1. Adding\s
+                        2. Subtraction\s
+                        
+                        """
+        );
     }
 }
